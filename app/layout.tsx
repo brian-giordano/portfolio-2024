e0a404d1f393bf677ec2d47f8de46aa51d97172c
Brian Giordano <bgiordano84@gmail.com>
2026-05-19 12:42:14 -0400

    Fix double-firing GA, Add code for GTM tags.

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,7 +1,7 @@
 import { Poppins, Oswald, Montserrat } from "next/font/google";
 import type { Metadata, Viewport } from "next";
 import { Analytics } from "@vercel/analytics/react";
-import GoogleAnalytics from "@/components/GoogleAnalytics";
+import Script from "next/script";
 import "./globals.css";
 import "../components/ui/Button.module.css";
 
@@ -185,7 +185,25 @@ export default function RootLayout({
         />
       </head>
       <body>
-        <GoogleAnalytics />
+        {/* Google Tag Manager (noscript) */}
+        <noscript>
+          <iframe 
+            src="https://www.googletagmanager.com/ns.html?id=GTM-TGBP82DW"
+            height="0" 
+            width="0" 
+            style={{ display: 'none', visibility: 'hidden' }}
+          />
+        </noscript>
+        {/* Google Tag Manager */}
+        <Script id="gtm" strategy="afterInteractive">
+          {`
+            (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+            new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+            j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+            'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+            })(window,document,'script','dataLayer','GTM-TGBP82DW');
+          `}
+        </Script>
         {children}
         <Analytics />
       </body>
